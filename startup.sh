0658a0c4bc96d3289ebfca9e52e8f1063646597e
Muzzino-orig <75335040+Muzzino-orig@users.noreply.github.com>
2021-01-20 17:56:33 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ openvpn --mktun --dev tap_softet
 sleep 1
 brctl addif br0 tap_softet
 
-vpnserver start >&/dev/null
+/root/vpnserver execsvc #start >&/dev/null
 
 brctl delif br0 tap_softet
 openvpn --rmtun --dev tap_softet
